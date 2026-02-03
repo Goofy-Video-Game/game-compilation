0fe1160cf4c9a7c0bd0c67f5d0052ae7ce642828
Goofy-Video-Game <Mattosdragon@gmail.com>
2026-02-03 22:32:49 +0000

    commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import game_code as ff
+
+def main():
+
+    # Dictionary of functions
+    functionscall = {
+        "talk to a person":ff.talk_to_a_person,
+        "caesar cipher": ff.caesar_cipher,
+        "fibonacci sequence": ff.fibonacci_sequence,
+        "fizzbuzz": ff.fizzbuzz,
+    }
+
+    # Asks what function to run then runs it
+    chosen_function = input(
+        "enter one of the following (case sensitive):\n"
+        "  'talk to a person'\n"
+        "  'caesar cipher'\n"
+        "  'fibonacci sequence'\n"
+        "  'fizzbuzz'\n\n"
+    ).lower()
+
+    if chosen_function in functionscall:
+        print(functionscall[chosen_function]())
+
+
+if __name__ == "__main__":
+    main()
